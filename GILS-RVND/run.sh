6657f6a4d449b69ac36f3ed6cb01884c8736a5c7
Victor Mororo <victormororo@gmail.com>
2020-12-30 20:40:18 -0300

    new benchmark files from 'francisccdn/TSP'

---
+++ GILS-RVND/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "--TSP Benchmark--"
+echo "--TSP Benchmark--" > ./benchmark/bm.txt
+
+make
+
+k=1
+for instance in instances/*; do
+	echo $instance >> ./benchmark/bm.txt
+
+	echo "Running $instance"
+	echo "Instance $k of 67" 
+
+	for i in {1..1}; do
+		./tsp ${instance} | grep 'COST\|TIME' | awk "{print $1}" >> ./benchmark/bm.txt
+	done
+
+	k=$(($k + 1))
+done
+
+echo "-" >> ./benchmark/bm.txt
+
+echo "Running bm.py to compute averages..."
+
+cd benchmark
+python3 bm.py
+
+echo "Finishing up summary..."
+python3 summarycount.py
+cd ..
+
+echo "Benchmark completed."
